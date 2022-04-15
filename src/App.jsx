3a518093e9c33c8b8c2ca1c9e900592b0244a5e8
Jairus Patrick Vallon <jpatrickvallon@gmail.com>
2022-04-15 15:06:29 +0800

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900">
+        React and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
